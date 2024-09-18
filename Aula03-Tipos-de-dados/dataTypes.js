90faf8d6872df7366012d91327dadcf9414b1716
JefersonRodrigues21 <dev.geekjs@gmail.com>
2024-09-17 23:31:34 -0300

    aula 03

---
+++ Aula03-Tipos-de-dados/dataTypes.js
@@ -0,0 +1,9 @@
+const nome = Bia;
+const idade = 24;
+let temCabeloCurto = true;
+console.log("Bia tem cabelo curto? ", temCabeloCurto);
+
+console.log("5 meses depois... ");
+
+temCabeloCurto = false;
+console.log("Bia tem cabelo curto?", temCabeloCurto);
