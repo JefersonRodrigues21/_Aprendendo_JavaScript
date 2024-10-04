dd4d86c28b97ce69eed0ff6515e9c3d681bf004e
JefersonRodrigues21 <dev.geekjs@gmail.com>
2024-10-04 15:17:40 -0300

    Aula09 fim

--- Aula09-Funções/functions.js
+++ Aula09-Funções/functions.js
@@ -1,14 +1,22 @@
-// Função sem parâmetros, e sem returno
-//function imprimir (){
-//    console.log("Olá Mundo!!!");
+//Função sem parâmetros, e sem returno
+//function imprimir (impressao){
+//    console.log(impressao);
 //}
 
-//imprimir();
+//***************************************************
 
-function calcularSoma (valor1, valor2){
-    return valor1 + valor2;
+const imprimir = function(impressao){
+    console.log(impressao);
 }
+imprimir("Olá Mundo!");
 
+//function calcularSoma (valor1, valor2){
+//    return valor1 + valor2;
+//}
+//****************************************************
 const valor1 = 2;
 const valor2 = 4;
-console.log(`A soma entre ${valor1} e ${valor2} é ${calcularSoma(valor1, valor2)}`);
+const calcularSoma = function(valor1, valor2){
+    return valor1 + valor2;
+}
+imprimir(`A soma entre ${valor1} e ${valor2} é ${calcularSoma(valor1, valor2)}`);
